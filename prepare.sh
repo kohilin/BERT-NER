a08387ad988598acc818a6b7f627f94e16e1efc8
KOHI <KOHI@ibm.com>
2020-04-25 22:46:56 +0000

    prepare.sh

---
+++ prepare.sh
@@ -0,0 +1,6 @@
+
+rm -rf bert
+git clone https://github.com/google-research/bert
+
+wget https://storage.googleapis.com/bert_models/2018_10_18/cased_L-12_H-768_A-12.zip
+unzip cased_L-12_H-768_A-12.zip
